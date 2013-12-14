081737ca02a1176348a4e38ccaccadb9e78163b6
j10438 <j10438@katsuo.3f.j.kisarazu.ac.jp>
2013-12-14 14:23:16 +0900

    add past time

--- prog-c/031/hello.c
+++ prog-c/031/hello.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(void)
 {
+  time_t start,end;
+
+  start = time(NULL);
   printf("Hello, world!\n");
+  end = time(NULL);
+  printf("Past time = %d[sec]\n", end - start);
+
   return EXIT_SUCCESS;
 }
