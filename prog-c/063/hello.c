dc9e280067f48743cc788b4f4042f9a6ceb49f49
j11413 <j11413@engawa.3f.j.kisarazu.ac.jp>
2013-12-14 14:54:05 +0900

    Change big 'Hello worldgit diff'

--- prog-c/063/hello.c
+++ prog-c/063/hello.c
@@ -3,6 +3,11 @@
 
 int main(void)
 {
-	printf("Hello, word!\n");
+	printf("H  H EEEE L    L     OO     W   W  OO  RRR  L    DDD  !!\n");
+	printf("H  H E    L    L    O  O    W W W O  O R  R L    D  D !!\n");
+	printf("HHHH EEEE L    L    O  O    W W W O  O RRR  L    D  D !!\n");
+	printf("H  H E    L    L    O  O    W W W O  O R R  L    D  D   \n");
+	printf("H  H EEEE LLLL LLLL  OO      W W   OO  R  R LLLL DDD  !!\n");
+	
 	return EXIT_SUCCESS;
 }
