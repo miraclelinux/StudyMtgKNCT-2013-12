3ba3537319186626222c751080371be4eeab55ca
j10422 <j10422@ootoro.3f.j.kisarazu.ac.jp>
2013-12-14 14:58:34 +0900

    Add function 'my_pow()'.

--- prog-c/022/hello.c
+++ prog-c/022/hello.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double my_pow(double n, int i)
+{
+  int j;
+  double sum = 1.00000;
+
+  for(j = 0; j < i; j++)
+    sum *= n;
+
+  return (sum);
+}
+
 int main(void)
 {
-	prIntf("Hello, world!\n'):
-	return EXIT_SUCCESS;
+  int i;
+
+  printf("Hello, world! %f  \n", my_pow(3.141592, 9));
+  return EXIT_SUCCESS;
 }
